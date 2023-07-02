ccd7b83bde037b36605d40b6f12029ff6ac08609
Slava <bogachevv_r@mail.ru>
2023-07-02 16:26:40 +0300

    - connected postgresql - created a Products class model - created a Categories class model - created a Descriptions class model - created an Images class model - created a GroupsCharacteristics class model - created a Fields class model - created a Producer class model

--- EleDev/settings.py
+++ EleDev/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'EleDev.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'EleDev',
+        'USER': 'postgres',
+        'PASSWORD': "",
+        'HOST': 'localhost',
+        'PORT': '5555',
+     }
 }
 
 
--- store/models.py
+++ store/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
 # Create your models here.
+DEFAULT_DISTINCTIVE_FEATURES = {
+    "color": "",
+    "diagonal": "",
+    "memory": "",
+    "volume": ""
+}
+
+
+class Products(models.Model):
+    title = models.CharField(max_length=50)
+    producer = models.ForeignKey('Producer', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    count = models.PositiveIntegerField()
+    feedback = models.PositiveIntegerField()
+    product_code = models.SmallIntegerField(primary_key=True)
+    distinctive_features = models.JSONField(default=DEFAULT_DISTINCTIVE_FEATURES)
+
+    def __str__(self):
+        return self.title
+
+
+class Categories(models.Model):
+    title = models.CharField(max_length=50)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Descriptions(models.Model):
+    description = models.TextField(null=False)
+    product = models.OneToOneField('Products', on_delete=models.CASCADE)
+
+
+class Images(models.Model):
+    electronic_devices = models.ForeignKey('Products', on_delete=models.CASCADE)
+
+
+class GroupsCharacteristics(models.Model):
+    description = models.ForeignKey('Descriptions', on_delete=models.CASCADE)
+    name_group = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name_group
+
+
+class Fields(models.Model):
+    name = models.CharField(max_length=20)
+    value = models.CharField(max_length=20)
+    gc = models.ForeignKey('GroupsCharacteristics', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Producer(models.Model):
+    logo = models.ImageField()
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
